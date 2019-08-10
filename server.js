3a5d93147b6c64bc288175414b7760fecedb8ab2
Joe Barnes <joebarnes333@gmail.com>
2019-08-09 19:10:18 -0700

    updated profile routes

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const express = require("express");
+const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
 const app = express();
 
-require('dotenv').config();
+require("dotenv").config();
 
 //DB config
 // const db = require('./config/keys').mongoURI;
 
-//db connection 
+//db connection
 // const uri = process.env.mongoURI2;
 // mongoose.connect(db, {useNewUrlParser: true, useCreateIndex: true});
 // const connection = mongoose.connection;
@@ -18,20 +21,29 @@ require('dotenv').config();
 // 	console.log('MongoDB database connection established successfully');
 // });
 
-//db connection 2
-const db = process.env.mongoURI
-mongoose
-    .connect(db, {useNewUrlParser: true, useCreateIndex: true})
-    .then(() => console.log('mongoDB connected'))
-    .catch(err => console.log(err))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('hello Joe'));
+//db connection 2
+const db = process.env.mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true, useCreateIndex: true })
+  .then(() => console.log("mongoDB connected"))
+  .catch(err => console.log(err));
+
+//passport middleware
+app.use(passport.initialize());
+
+//passport config
+require("./config/passport")(passport);
+
+// app.get("/", (req, res) => res.send("hello Joe"));
 
 //use routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`server has started on port ${port}`))
+app.listen(port, () => console.log(`server has started on port ${port}`));
---
+++ validation/is-empty.js
@@ -0,0 +1,9 @@
+//function that will check for empty items
+
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
+
+module.exports = isEmpty;
