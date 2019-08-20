ccd54087a3ef505c7c0fc35ac7bcc3cad61d676c
Joe Barnes <joebarnes333@gmail.com>
2019-08-19 22:51:14 -0700

    add react and front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/layout/NavBar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello </h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
