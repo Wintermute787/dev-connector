e3a6731e6680140c617e5f6c17a137dd00e257df
Joe Barnes <joebarnes333@gmail.com>
2019-08-20 22:47:28 -0700

    add more login info

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser } from "./actions/authActions";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 import "./App.css";
 
@@ -21,6 +21,16 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   //set user and isauthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  //check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    //logout user
+    store.dispatch(logoutUser);
+    // TODO cLear current profile
+    //redirect to login
+    window.location.href = "/login";
+  }
 }
 
 class App extends Component {
