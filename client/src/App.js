5d94e1dfc1c9514d41a52ff248d6fde92be7e02b
Joe Barnes <joebarnes333@gmail.com>
2019-09-10 23:28:13 -0700

    add comments

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,7 @@ import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import NotFound from "./components/not-found/NotFound";
 import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
 
 //check for token
 if (localStorage.jwtToken) {
@@ -87,6 +88,9 @@ class App extends Component {
                   path="/add-education"
                   component={AddEducation}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/post/:id" component={Post} />
               </Switch>
               <Switch>
                 <PrivateRoute exact path="/feed" component={Posts} />
